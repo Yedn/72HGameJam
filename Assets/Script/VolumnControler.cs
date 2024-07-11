9ca139f3cd88b7c737344cdf2a3c201a6a533614
priceEXE <152944571+priceEXE@users.noreply.github.com>
2024-07-11 09:29:47 +0800

    修改

--- Assets/Script/VolumnControler.cs
+++ Assets/Script/VolumnControler.cs
@@ -11,7 +11,7 @@ public class VolumnControler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-      //  DontDestroyOnLoad(gameObject);
+     //   DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
@@ -20,8 +20,10 @@ public class VolumnControler : MonoBehaviour
       BGMAudio.volume = volumnslider.value;  
     }
 
- //   public void UpDateGameObject()  {
-  //    GameObject audioSlider = GameObject.FindGameObjectWithTag("gameSettingUI").transform.Get
-   // }
+   // public void UpDateGameObject()  {
+   //   GameObject audioSlider = GameObject.FindGameObjectWithTag("gameSettingUI").transform.GetChild(0).GetChild(0).gameObject;
+   //   PlayerPrefs.SetFloat("audioVolume",audioSlider.GetComponent<Slider>().value);
+   //   print(PlayerPrefs.GetFloat("audioVolume"));
+    //} 
 
 }

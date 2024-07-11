d21afa1481fd6d2e2cc41d9c72b432da1f196b71
priceEXE <152944571+priceEXE@users.noreply.github.com>
2024-07-11 10:43:31 +0800

    内外音量统一

--- Assets/Script/VolumnControler.cs
+++ Assets/Script/VolumnControler.cs
@@ -1,29 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class VolumnControler : MonoBehaviour
 {
-    public AudioSource BGMAudio;
-    public Slider volumnslider;
 
     // Start is called before the first frame update
     void Start()
     {
-     //   DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-      BGMAudio.volume = volumnslider.value;  
+      UpDateGameObject();
+      GetGameObjectValue();
     }
 
-   // public void UpDateGameObject()  {
-   //   GameObject audioSlider = GameObject.FindGameObjectWithTag("gameSettingUI").transform.GetChild(0).GetChild(0).gameObject;
-   //   PlayerPrefs.SetFloat("audioVolume",audioSlider.GetComponent<Slider>().value);
-   //   print(PlayerPrefs.GetFloat("audioVolume"));
-    //} 
+    public void UpDateGameObject()  {
+      UnityEngine.SceneManagement.Scene activeScene = SceneManager.GetActiveScene();
+      if(activeScene.name == "Mainmenu")
+      {
+        GameObject BGM = GameObject.Find("BGM").transform.gameObject;
+        GameObject audioSlider = GameObject.FindGameObjectWithTag("GameSettingUI").transform.GetChild(0).GetChild(0).gameObject; 
+        PlayerPrefs.SetFloat("audioVolume",audioSlider.GetComponent<Slider>().value);
+        BGM.GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("audioVolume");
+        print(PlayerPrefs.GetFloat("audioVolume"));
+      }
+    
+    } 
+
+    public void GetGameObjectValue()  {
+      UnityEngine.SceneManagement.Scene activeScene = SceneManager.GetActiveScene();
+      if(activeScene.name == "test1")
+      {
+        GameObject BGM = GameObject.Find("BGM").transform.gameObject;
+        BGM.GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("audioVolume");
+      }
+    }
 
 }
